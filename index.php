88382db228da0284c909ad05112f657bf06b47c1
adrian szczotka <zapasowyadi@gmail.com>
2023-10-06 18:35:14 +0200

    Dodanie stron,admin,pytania,plansza,index

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mechaniczna Pomarańcza</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h1>Mechaniczna Pomarańcza</h1>
+        <ul class="nav nav-underline">
+        <li class="nav-item">
+            <a class="nav-link" href="pytania/pytania.php">Pytania</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="admin/admin.php">Admin</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="plansza/plansza.php">Plansza</a>
+        </li>
+        </ul>
+    </body>
+    <style>
+    h1, ul{
+        text-align: center;
+        justify-content: center;
+    }
+    li{
+        font-size: xx-large;
+    }
+    </style>
+</html>
+<!-- Strona po to ze jak wejdzesz to jest po prostu prościej się poruszać po plikach -->
