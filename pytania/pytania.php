f2530631e7bbb966664095d24668dbe178d216fa
adrian szczotka <zapasowyadi@gmail.com>
2023-10-10 18:54:49 +0200

    pytania cały frontend (chyba)

--- pytania/pytania.php
+++ pytania/pytania.php
@@ -33,7 +33,7 @@
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
-    opacity: 0.55;
+    opacity: 0.35;
     }
 
     /* Position text in the middle of the page/image */
@@ -47,20 +47,52 @@
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: 2;
-    width: 80%;
+    width: 85%;
     padding: 20px;
     text-align: center;
     border-radius: 5px;
     }
+    #stats{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        justify-content: space-around;
+    }
+    h3{
+        color: rgb(100, 100, 100);
+    }
+    #timer{
+        border: 4px solid orangered;
+        color: darkorange;
+        margin-top: 20px !important;
+        width: 50%;
+        margin: auto;
+    }
+    #time{
+        color: rgb(70,70,70);
+    }
+
+
+
+
 </style>
 <body>
     <div class="bg-image"></div>
 
     <div class="bg-text">
-    <h1>Pytanie(przykład):</h1>
-    <img src="../baza_pytania/chemia/poziom_2/c1.jpg" width="50%" style="padding-bottom:10px;">
-    <h1>Poprawna odpwiedź(przykład):</h1>
+    <div id="stats">
+        <div id="druzyna"><h2>Nr drużyny:</h2><h3 id="druzyna-data">1</h3></div>
+        <hr class="border border-warning border-3 opacity-100" >
+        <div id="poziom"><h2>Poziom trudności:</h2><h3 id="poziom-data">1</h3></div>
+        <hr class="border border-warning border-3 opacity-100" >
+        <div id="kategoria"><h2>Kategoria:</h2><h3 id="poziom-data">NIGGER</h3></div>
+    </div>
+    <hr class="border border-warning border-3 opacity-100" >
+    <h1>Pytanie:</h1>
+    <img src="../baza_pytania/chemia/poziom_2/c1.jpg" width="50%">
+    <h1>Poprawna odpwiedź:</h1>
     <img src="../baza_pytania/chemia/poziom_2/co1.jpg" width="20%">
+    <div id="timer"><h1>Pozostały czas:</h1><h2 id="time">30</h2></div>
     </div>
 </body>
 
