6fbb9e404476ddde1beda50a743a8571fb96c9f7
adrian szczotka <zapasowyadi@gmail.com>
2023-10-11 16:28:31 +0200

    test na pytania php

---
+++ admin/admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin - Mechaniczna pomarańcza</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link rel="shortcut icon" href="../grafika/favicon/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        <form>
+        <div class="btn-group btn-group-lg" role="group" aria-label="Large button group">
+            <button type="button" class="btn btn-primary">Chemia</button>
+            <button type="button" class="btn btn-primary">Informatyka</button>
+            <button type="button" class="btn btn-primary">Fizyka</button>
+            <button type="button" class="btn btn-primary">Matematyka</button>
+            <button type="button" class="btn btn-primary">Technika</button>
+            <button type="button" class="btn btn-primary">Niespodzianka</button>
+        </div>
+        <div class="btn-group btn-group-lg" role="group" aria-label="Large button group">
+            <button type="button" class="btn btn-primary">za 2 punkty</button>
+            <button type="button" class="btn btn-primary">za 1 punkt</button>
+        </div>
+        <div id="demo"></div></div>
+        </form>
+    </body>
+</html>
